301dc6f71941643f1ca63160fe8012ef23950f2b
Jordan Crouch <107533237+jordancrouch@users.noreply.github.com>
2025-02-25 18:57:59 +0000

    Test loading separate HTML using JavaScript

---
+++ src/assets/js/site.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", function () {
+  this.document.getElementById("characters").addEventListener("click", (e) => {
+    e.preventDefault();
+    this.fetch("/test")
+      .then((response) => {
+        if (response.ok) {
+          return response.text();
+        }
+        throw response;
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  });
+});
