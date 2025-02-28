cf51237b31457fa1ef031322575407c8c7480b65
Jordan Crouch <107533237+jordancrouch@users.noreply.github.com>
2025-02-28 21:05:47 +0000

    Test: fetch character page content and update on index page

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,17 +1,32 @@
-window.addEventListener("load", function () {
-  /*
-  this.document.getElementById("characters").addEventListener("click", (e) => {
-    e.preventDefault();
-    this.fetch("/test")
-      .then((response) => {
-        if (response.ok) {
-          return response.text();
-        }
-        throw response;
-      })
-      .then((data) => {
-        console.log(data);
-      });
-  });
-  */
+window.addEventListener("DOMContentLoaded", () => {
+  const charactersButton = this.document.getElementById("characters-button");
+
+  const stringToHTML = (text) => {
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(text, "text/html");
+    return doc.body;
+  };
+
+  if (charactersButton) {
+    const charactersLink = charactersButton.getAttribute("href");
+    charactersButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.fetch(charactersButton.getAttribute("href"))
+        .then((response) => {
+          if (response.ok) {
+            return response.text();
+          }
+          throw response;
+        })
+        .then((text) => {
+          let html = stringToHTML(text);
+          let newHeader = html.querySelector("#header");
+          let newMain = html.querySelector("#main");
+          let currentHeader = document.getElementById("header");
+          currentHeader.replaceWith(newHeader);
+          let updatedHeader = document.getElementById("header");
+          updatedHeader.insertAdjacentElement("afterend", newMain);
+        });
+    });
+  }
 });
