026095704a1dc6f95adbc57c0f2fe6cd01e6b867
Jordan Crouch <107533237+jordancrouch@users.noreply.github.com>
2025-02-28 22:35:32 +0000

    Feat: add character names, event listener, and remove caching from http-server

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,15 +1,33 @@
 window.addEventListener("DOMContentLoaded", () => {
+  /* Set up variables */
+  // Get the 'choose a character' button
   const charactersButton = this.document.getElementById("characters-button");
+  const characters = document.getElementsByClassName("character");
 
+  // Function to convert a string to HTML
   const stringToHTML = (text) => {
     let parser = new DOMParser();
     let doc = parser.parseFromString(text, "text/html");
     return doc.body;
   };
 
-  if (charactersButton) {
+  const getCharacterNames = () => {
+    Array.from(characters).forEach((character) => {
+      character.addEventListener("click", (e) => {
+        // Get the character name
+        const characterName = e.target.getAttribute("data-name");
+        console.log(characterName);
+      });
+    });
+  };
+
+  // Fetch the characters page and replace the current page with the new page
+  if (charactersButton !== null) {
+    // Get the current URL
     const currentURL = window.location.href;
+    // Get the href attribute of the characters button
     let charactersLink = charactersButton.getAttribute("href");
+    // Check if the current URL is localhost, else add repo path to the characters link
     if (!currentURL.includes("localhost")) {
       charactersLink = "/middle-earth-typing-trials" + charactersLink;
     }
@@ -30,7 +48,13 @@ window.addEventListener("DOMContentLoaded", () => {
           currentHeader.replaceWith(newHeader);
           let updatedHeader = document.getElementById("header");
           updatedHeader.insertAdjacentElement("afterend", newMain);
+        })
+        .then(() => {
+          getCharacterNames();
         });
     });
   }
+
+  // Get the character names
+  getCharacterNames();
 });
