4c7f42d0c9d356d1f11bd9784e7be501019d1c68
Jordan Crouch <107533237+jordancrouch@users.noreply.github.com>
2025-03-02 19:13:14 +0000

    Feat: add letter progress functionality

--- assets/js/typing-test.js
+++ assets/js/typing-test.js
@@ -9,12 +9,16 @@ export class TypingTest {
     this.intervalTime = 2000;
     this.eventListeners();
     this.setFocusInterval();
+    this.inputEvents = 0;
+    this.wordIndex = 0;
+    this.letterIndex = 0;
   }
 
-  // Add event listeners to inner container.
+  // Add event listeners.
   eventListeners() {
     this.testInner.addEventListener("click", this.focusOnInput);
     this.testInner.addEventListener("touch", this.focusOnInput);
+    this.textInput.addEventListener("input", this.onInputChange);
   }
 
   // Event handler to focus back on input element.
@@ -23,19 +27,19 @@ export class TypingTest {
     this.checkInputFocus();
   };
 
+  // Set interval to check if input is in focus.
   setFocusInterval() {
     window.setInterval(() => {
-      console.log("interval has completed.");
       this.checkInputFocus();
     }, this.intervalTime);
   }
 
+  // Check if input is in focus.
   checkInputFocus() {
     let activeElement = document.activeElement;
     let input = this.textInput;
     let textContainer = this.textContainer;
     if (activeElement === input) {
-      console.log("input is focused");
       if (textContainer.classList.contains("blur")) {
         textContainer.classList.remove("blur");
       }
@@ -49,6 +53,77 @@ export class TypingTest {
 
       if (this.focusWarning.classList.contains("opacity-0")) {
         this.focusWarning.classList.remove("opacity-0");
+      }
+    }
+  }
+
+  // Event handler for input change.
+  onInputChange = (e) => {
+    if (e !== undefined) {
+      console.log(e);
+      let input = "";
+      // If text input, set text data to variable.
+      if (e.inputType === "insertText") {
+        if (e.data !== " ") {
+          input = e.data;
+        } else if (e.data === " ") {
+          input = "space";
+        }
+        // If backspace, set input as 'delete' and decrement letter index.
+      } else if (e.inputType === "deleteContentBackward") {
+        input = "delete";
+        // Prevent negative index.
+        if (this.letterIndex !== 0) {
+          this.letterIndex--;
+        }
+      }
+      this.textProgress(input);
+      console.log(`Input: ${input}`);
+      console.log(`Word index: ${this.wordIndex}`);
+      console.log(`Letter index: ${this.letterIndex}`);
+    }
+  };
+
+  // Functin to track progress of text.
+  textProgress(input) {
+    let text = this.textContainer;
+    let words = text.childNodes;
+    let wordIndex = this.wordIndex;
+
+    // Iterate over the words in the text container.
+    for (const [key, value] of Object.entries(words)) {
+      // Set the current word to active.
+      if (Number(key) === wordIndex) {
+        value.classList.add("active");
+
+        let letters = value.childNodes;
+        let wordLength = letters.length;
+        let letterIndex = this.letterIndex;
+        // Iterate over the letters in the current word.
+        for (const [key, value] of Object.entries(letters)) {
+          if (Number(key) === letterIndex) {
+            // Check to make sure that the current letter index is less than the word length.
+            if (Number(key) <= wordLength) {
+              let letter = value.innerHTML;
+              // Correct input, add correct class and increment letter index.
+              if (letter === input) {
+                value.classList.add("correct");
+                this.letterIndex++;
+                // If input is delete, remove correct or incorrect class.
+              } else if (input === "delete") {
+                if (value.classList.contains("correct")) {
+                  value.classList.remove("correct");
+                } else if (value.classList.contains("incorrect")) {
+                  value.classList.remove("incorrect");
+                }
+                // If incorrect input, add incorrect class and increment letter index.
+              } else if (letter !== input) {
+                value.classList.add("incorrect");
+                this.letterIndex++;
+              }
+            }
+          }
+        }
       }
     }
   }
