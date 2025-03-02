1d1c4e8ba9d7dcd0a5dc875d6ab766c30bff2cd2
Jordan Crouch <107533237+jordancrouch@users.noreply.github.com>
2025-03-02 14:33:31 +0000

    Feat: wrap test quote letters and words

--- assets/js/site.js
+++ assets/js/site.js
@@ -3,33 +3,40 @@ import { Quotes } from "./quotes.js";
 
 window.addEventListener("load", () => {
   /* Set up variables */
-  // Get the 'choose a character' button
+  // Get the 'choose a character' button.
   const charactersButton = document.getElementById("characters-button");
+  // Get all the character cards.
   const characterCards = document.getElementsByClassName("character");
 
-  // Function to check if the current location is localhost
+  // Function to check if the current location is localhost.
   const isProduction = () => {
     return location.hostname !== "localhost";
   };
 
-  // Function to convert a string to HTML
+  // Function to convert a string to HTML.
   const stringToHTML = (text) => {
     let parser = new DOMParser();
     let doc = parser.parseFromString(text, "text/html");
     return doc.body;
   };
 
-  const getCharacterNames = () => {
-    Array.from(characterCards).forEach((character) => {
-      character.addEventListener("click", (e) => {
-        // Get the character name
-        const characterName = e.target.getAttribute("data-name");
-        console.log(characterName);
-      });
-    });
+  // Event handler to get character names.
+  const getCharacterNames = (e) => {
+    const characterName = e.target.getAttribute("data-name");
+    console.log(characterName);
+    // TODO: save selected character name to a variable.
   };
 
+  // Iterate over the character cards and add event listeners to each.
+  Array.from(characterCards).forEach((character) => {
+    character.addEventListener("click", getCharacterNames);
+    character.addEventListener("touch", getCharacterNames);
+    // TODO: add event listener for return/enter key press when character is focused.
+  });
+
   // Fetch the characters page and replace the current page with the new page
+  // if the characters button exists and is clicked.
+  // TODO: add touch and keypress event listeners.
   if (charactersButton !== null) {
     // Get the href attribute of the characters button
     let charactersLink = charactersButton.getAttribute("href");
@@ -62,8 +69,15 @@ window.addEventListener("load", () => {
   }
 
   // Get the character names
-  getCharacterNames();
+  // getCharacterNames();
   // const character = new Character("gandalf");
   // console.log(character);
   // console.log(character.getCharacterQuotes());
+
+  const quotes = new Quotes();
+
+  const textContainer = document.getElementById("text-container");
+  if (textContainer !== null) {
+    textContainer.innerHTML = quotes.processedQuotes;
+  }
 });
