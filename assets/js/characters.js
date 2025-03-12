5c0fdd69bbc385c03c467c1eac1741c844aea90e
Jordan Crouch <107533237+jordancrouch@users.noreply.github.com>
2025-03-12 23:10:02 +0000

    Feat: add enter key event listeners to select a character and to start the test

--- assets/js/characters.js
+++ assets/js/characters.js
@@ -13,31 +13,50 @@ export class Character {
     this.addEventListeners();
   }
 
-  // Function to add event listeners to character cards.
+  // Function to add event listeners to character cards and start button.
   addEventListeners() {
     // Iterate over the character cards and add event listeners to each.
     if (this.characterCards !== null) {
       Array.from(this.characterCards).forEach((character) => {
         character.addEventListener("click", this.getCharacterNames);
         character.addEventListener("touch", this.getCharacterNames);
-        // TODO: add event listener for key press (tab) when character is focused.
+        character.addEventListener("keydown", this.getCharacterNames);
       });
     }
 
     if (this.startButton !== null) {
       this.startButton.addEventListener("click", this.getCharacterQuotes);
       this.startButton.addEventListener("touch", this.getCharacterQuotes);
-      // TODO: add event listener for key press (enter/return) when character is selected.
+      this.startButton.addEventListener("keydown", this.getCharacterQuotes);
     }
   }
 
   // Event handler to get character names.
   getCharacterNames = (e) => {
     if (e !== undefined) {
-      const characterName = e.target.getAttribute("data-name");
-      this.name = characterName;
-      this.getCharacterID(characterName);
-      // TODO: save selected character name to a variable.
+      if (
+        e.type === "click" ||
+        e.type === "touchstart" ||
+        (e.type === "keydown" && e.key === "Enter")
+      ) {
+        const characterCards = this.characterCards;
+        if (!e.target.classList.contains("active")) {
+          Array.from(characterCards, (character) => {
+            if (
+              character.querySelector("picture").classList.contains("active")
+            ) {
+              character.querySelector("picture").classList.remove("active");
+            }
+          });
+          e.target.classList.add("active");
+        }
+        const characterName = e.target.getAttribute("data-name");
+        this.name = characterName;
+        this.getCharacterID(characterName);
+      }
+    } else {
+      e.prevrntDefault();
+      e.stopPropagation();
     }
   };
 
@@ -63,59 +82,74 @@ export class Character {
     }
   }
 
-  async getCharacterQuotes() {
-    // Set # of characters to 0.
-    let size = 0;
+  // Function to fetch character quotes from the One API.
+  async getCharacterQuotes(e) {
+    if (e !== undefined) {
+      if (
+        e.type === "click" ||
+        e.type === "touchstart" ||
+        (e.type === "keydown" && e.key === "Enter")
+      ) {
+        // Set # of characters to 0.
+        let size = 0;
 
-    // If one character has been selected.
-    if (typeof this.id === "string") {
-      size = 1;
-      // Else if multiple characters have been selected.
-    } else if (Array.isArray(this.id)) {
-      size = Object.keys(this.id).length;
-    }
-    const API_KEY = ONE_API;
-    const headers = new Headers({
-      Authorization: `Bearer ${API_KEY}`,
-    });
-    const options = {
-      headers: headers,
-    };
-
-    const fetchQuotes = async (name, id) => {
-      try {
-        const url = `https://the-one-api.dev/v2/character?name=${id}`;
-        const response = await fetch(url, options);
-        const data = await response.json();
-        const characterID = data.docs[0]._id;
-        const characterURL = `https://the-one-api.dev/v2/character/${characterID}/quote`;
-        const quoteResponse = await fetch(characterURL, options);
-        const quoteData = await quoteResponse.json();
-        return new getQuotesInstance(quoteData);
-      } catch (error) {
-        console.error(error.message);
-
-        // Fallback to local JSON data.
-        // TODO: create/update object property to show icon
-        // front-end that local data is being used.
-        try {
-          const localResponse = await fetch(`./assets/js/data/${name}.json`);
-          const localData = await localResponse.json();
-          console.log("Using local data:", localData);
-          return new getQuotesInstance(localData);
-        } catch (localError) {
-          console.error(localError.message);
+        // If one character has been selected.
+        if (typeof this.id === "string") {
+          size = 1;
+          // Else if multiple characters have been selected.
+        } else if (Array.isArray(this.id)) {
+          size = Object.keys(this.id).length;
         }
-      }
-    };
+        const API_KEY = ONE_API;
+        const headers = new Headers({
+          Authorization: `Bearer ${API_KEY}`,
+        });
+        const options = {
+          headers: headers,
+        };
 
-    if (size === 1) {
-      return await fetchQuotes(this.name, this.id);
-    } else if (size > 1) {
-      const promises = Object.keys(this.id).map((key) =>
-        fetchQuotes(this.name, this.id[key]),
-      );
-      return await Promise.all(promises);
+        const fetchQuotes = async (name, id) => {
+          try {
+            const url = `https://the-one-api.dev/v2/character?name=${id}`;
+            const response = await fetch(url, options);
+            const data = await response.json();
+            const characterID = data.docs[0]._id;
+            const characterURL = `https://the-one-api.dev/v2/character/${characterID}/quote`;
+            const quoteResponse = await fetch(characterURL, options);
+            const quoteData = await quoteResponse.json();
+            return new getQuotesInstance(quoteData);
+          } catch (error) {
+            console.error(error.message);
+
+            // Fallback to local JSON data.
+            // TODO: create/update object property to show icon
+            // front-end that local data is being used.
+            try {
+              const localResponse = await fetch(
+                `./assets/js/data/${name}.json`,
+              );
+              const localData = await localResponse.json();
+              console.log("Using local data:", localData);
+              return new getQuotesInstance(localData);
+            } catch (localError) {
+              console.error(localError.message);
+            }
+          }
+        };
+
+        // Fetch quotes based on the number of characters selected.
+        if (size === 1) {
+          return await fetchQuotes(this.name, this.id);
+        } else if (size > 1) {
+          const promises = Object.keys(this.id).map((key) =>
+            fetchQuotes(this.name, this.id[key]),
+          );
+          return await Promise.all(promises);
+        }
+      } else {
+        e.prevrntDefault();
+        e.stopPropagation();
+      }
     }
   }
 }
