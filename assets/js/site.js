788686346192b70231b0956ce0fca0509d527663
Jordan Crouch <107533237+jordancrouch@users.noreply.github.com>
2025-03-15 17:20:37 +0000

    Feat: add settings popover

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,5 +1,28 @@
 import { Character } from "./characters.js";
 
 window.addEventListener("load", () => {
+  // Custom popover close button
+  const popover = document.getElementById("settings-popover");
+  const popoverClose = document.getElementById("popover-close");
+
+  const closePopover = (e) => {
+    if (
+      e.type === "click" ||
+      e.type === "touchstart" ||
+      (e.type === "keydown" && e.key === "Enter")
+    ) {
+      e.preventDefault();
+      if (popover.matches(":popover-open")) {
+        popover.hidePopover();
+      }
+    }
+  };
+
+  if (popoverClose) {
+    popoverClose.addEventListener("click", closePopover);
+    popoverClose.addEventListener("touch", closePopover);
+    popoverClose.addEventListener("keydown", closePopover);
+  }
+
   let characters = new Character();
 });
